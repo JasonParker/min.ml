96129994de9f78b49df04d6da09487e54aa12fc6
jasonparker <jason.parker2@hcahealthcare.com>
2022-03-23 22:21:26 -0500

    Prediction workload skeleton

---
+++ src/modeling/predict.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from src.data.collect import collect_data
+from src.data.process import preprocessing
+
+
+def prediction_workload(**kwargs):
+    print(f"Prediction workload initiated at {str(datetime.utcnow())[0:19]}")
+    ## Fetch data
+    ## Splitting/caching
+    ## Preprocessing
+    ##  - Valuable to have a preprocessing function that can
+    ##      apply the same transformations to the data in both
+    ##      training and prediction
+    ## Train model
+    ## Evaluate
+    ## Store model artifacts
+
+    data = collect_data(scenario = 'prediction')
+    data = preprocessing(data)
+    
+    return "Great predictions"
